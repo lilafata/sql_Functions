e2a4979820a2d534b9b70c4a5715d924b326676d
lilafata <40909219+lilafata@users.noreply.github.com>
2025-01-29 09:17:50 -0600

    Add files via upload

--- sql_Functions.py
+++ sql_Functions.py
@@ -14,30 +14,7 @@
 # 1) This program was executed using the IDLE Python 3.7 Shell on a Windows 10 laptop
 # 2) Need to manually remove the db file prior to rerunning this script due to unique ID error
 # 3) Will try looking into removing db file automatically in script
-#
-#
-# OUTPUT:
-# Python 3.7.0b3 (v3.7.0b3:4e7efa9c6f, Mar 29 2018, 18:42:04) [MSC v.1913 64 bit (AMD64)] on win32
-# Type "copyright", "credits" or "license()" for more information.
-# >>> 
-#  RESTART: C:\Users\Racter\Desktop\Lila\Resumes\LatestResume\Resumes2024\version2\Wolverine\sql_Functions.py 
-#
-# sqlite3 student_database.db file with Students table successully created and connected!
-#
-# Students Table with records successfully inserted!
-# Students Table:
-# (11, 'Jane Jones', 19, 'Economics')
-# (12, 'Michael Calhoun', 18, 'Electronics Engineering')
-# (13, 'Laura Hill', 17, 'Marketing')
-# (14, 'Tom Smith', 20, 'Information Technology')
-#
-# Students Table with one record successfully updated!
-# Students Table Updated:
-# (11, 'Jane Jones', 19, 'Economics')
-# (12, 'Michael Calhoun', 18, 'Electronics Engineering')
-# (13, 'Laura Hill', 17, 'Nursing')
-# (14, 'Tom Smith', 20, 'Information Technology')
-# >>> 
+# 4) Sample Output 'sql_Functions_SAMPLEOutput.txt is included
 #
 ##########################################################################################################
 
